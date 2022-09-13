f356d22c094372139a350fcf7f53ad5a61cf5d5b
odinakazy <odinakasolomon4@gmail.com>
2022-09-13 07:08:23 -0700

    bit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,16 +7,14 @@
 
 int main(void)
 {
-	putchar('');
-	_putchar('p');
-	_putchar('u');
+_putchar('_');
+_putchar('p');
+_putchar('u');
 	_putchar('t');
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
-	return (0);
-
+return (0);
 }
