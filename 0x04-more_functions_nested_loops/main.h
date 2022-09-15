82893f51e800c0577b8e26e7acf46f182a65111b
odinakazy <odinakasolomon4@gmail.com>
2022-09-15 15:23:16 -0700

    took

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,10 +2,10 @@
 #define MAIN_H
 
 /**
-*main - header file for protoypes
-*void protoypes (void)
-*int protoypes (int)
-*/
+ *main - header file for protoypes
+ *void protoypes (void)
+ *int protoypes (int)
+ */
 
 void print_numbers(void);
 void print_most_numbers(void);
@@ -20,4 +20,4 @@ int _isupper(int);
 int _isdigit(int);
 int mul(int, int);
 
-#endif /* MAIN_H *
+#endif /* MAIN_H */
