28c102acb3a8352bc6cc5575ad1ece1fb654d967
odinakazy <odinakasolomon4@gmail.com>
2022-10-20 12:33:59 -0700

    good

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,8 +3,9 @@
  * bmain - function executed before main
  * Return: no return.
  */
-void _attribute_((constructor)) bmain()
+void __attribute__((constructor)) bmain()
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
 }
+
