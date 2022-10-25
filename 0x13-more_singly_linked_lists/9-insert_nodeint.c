e596a0b7ba7db2fd570005b7a6a288ddca5fdf57
odinakazy <odinakasolomon4@gmail.com>
2022-10-25 14:38:03 -0700

    task9 done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,47 +1,22 @@
 #include "lists.h"
+
 /**
- * insert_nodeint_at_index - This function inserts a
- * new node at given position
- * @head: The head node
- * @idx: The index of the list where new node is to be added
- * @n: The integer data of the new node
- * Return: The address of the new inserted node or NULL if failed
+ * _strcpy - Copy paste string
+ * @dest: destination
+ * @src: source
+ *
+ * Return: destination
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+
+char *_strcpy(char *dest, char *src)
 {
-	listint_t *node, *temp = *head;
-	size_t i = 0;
-
-	node = malloc(sizeof(listint_t));
-	if (!node)
-		return (NULL);
-
-	node->n = n;
-	node->next = NULL;
-
-	if (!*head && !idx)
-	{
-		*head = node;
-		return (node);
-	}
-
-	else if (!*head && idx)
-		return (NULL);
-
-	else if (!idx)
-	{
-		node->next = temp;
-		*head = node;
-		return (node);
-	}
-
-	while (i < (idx - 1))
-	{
-		temp = temp->next;
-		i++;
-	}
-	node->next = temp->next;
-	temp->next = node;
-
-	return (node);
+int inc = 0;
+while (*(src + inc) != '\0')
+{
+*(dest + inc) = *(src + inc);
+inc++;
 }
+*(dest + inc) = '\0';
+return (dest);
+}
+
